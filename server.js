76f3ba826cda331f596bc8e9722d7442306da750
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-04-03 22:02:50 -0700

    clean up

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // DEPENDENCIES
 const express = require('express')
-const methodOverride=require('method-override')
-const mongoose = require('mongoose')
+
 
 // CONFIGURATION
 require('dotenv').config()
@@ -9,26 +8,17 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
-app.use(express.static('public'))
-app.use(methodOverride('_method'))
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+
+
 
 
 
 
 
 app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
+  res.send('Welcome to an Awesome App about Books!')
 })
-// breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
 
-// bakers 
-const bakersController = require('./controllers/bakers_controller.js')
-app.use('/bakers', bakersController)
 
 // 404 Page
 app.get('*', (req, res) => {
@@ -39,6 +29,4 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => { console.log('connected to mongo: ', process.env.MONGO_URI) })
 
